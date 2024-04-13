92ca2b101c19b60b72d9e4c8467811eebb7bb540
Kourouma25 <166862706+Kourouma25@users.noreply.github.com>
2024-04-13 22:25:04 +0200

    Update main.py

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,8 @@
 # Écrivez votre code ici !
+prenom = "kourouma"
+age = 32
+print(f"je m'appelle {prenom} et j'ai {age} ans.")
+
+prenom = "kourouma"
+age = 32 + 10
+print(f"je m'appelle {prenom} et j'ai {age} ans.")
