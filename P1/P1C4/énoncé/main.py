37fa4115caf985ebd40391d11f1d3d2593ca627c
Kourouma25 <166862706+Kourouma25@users.noreply.github.com>
2024-04-13 14:41:08 +0200

    Update main.py

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,4 @@
-# Écrivez votre code ici !
+nourriture_preferee1 = "Glace"
+nouvelle_nourriture_preferee = "Pizza"
+nourriture_preferee2 = "pates"
+print(f"j'aime la {nourriture_preferee1}, {nouvelle_nourriture_preferee} et les {nourriture_preferee2}"
